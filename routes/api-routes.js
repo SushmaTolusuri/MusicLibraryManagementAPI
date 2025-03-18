77b86956816c011ee592125dd7a679dc47e8ad3a
SushmaTolusuri <30135057@students.southwales.ac.uk>
2025-03-18 19:56:59 +0300

    Added the endpoints interacting with the CRUD functions for the genre model

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,6 +15,9 @@ router.get('/',function (req,res){
 
 // Import controllers here
 import * as userController from "../controllers/userController.js"
+import * as genreController from "../controllers/genreController.js"
+
+
 import verifyToken from '../auth/auth.js'
 
 
@@ -33,6 +36,17 @@ router.route('/admin/readusers').get([verifyToken, userController.readUsers]);
 router.route('/admin/updateuser').put([verifyToken, userController.updateUser]);
 // -- Delete Route - POST
 router.route('/admin/deleteuser').post([verifyToken, userController.deleteUser]);
+
+
+// Genre Model Routes
+// -- Create Route - POST
+router.route('/admin/creategenre').post([verifyToken, genreController.createGenre]);
+// -- Read Route - GET
+router.route('/admin/readgenres').get([verifyToken, genreController.readGenres]);
+// -- Update Route - PUT
+router.route('/admin/updategenre').put([verifyToken, genreController.updateGenre]);
+// -- Delete Route - POST
+router.route('/admin/deletegenre').post([verifyToken, genreController.deleteGenre]);
 
 // Eport API routers. As it is the only export,
 // we make it the default
