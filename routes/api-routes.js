8ec530e38a3d8013a1e4e35dcb93901852141e15
SushmaTolusuri <30135057@students.southwales.ac.uk>
2025-03-18 05:36:46 +0300

    Added the base routes file

---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+// Filename: api-routes.js
+//Initialize express router
+import express from 'express'
+const router=express.Router()
+
+
+// Set Default API Response
+router.get('/',function (req,res){
+	res.json({
+		status: 'API is working',
+		message: 'Welcome to my Rest API'
+
+	});
+});
+
+// Import controllers here
+
+
+
+//define routes here
+
+
+// Eport API routers. As it is the only export,
+// we make it the default
+export default router;
