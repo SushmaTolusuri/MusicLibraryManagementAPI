2f0050444f1183fa999a490fa902a26edf7fe7f6
SushmaTolusuri <30135057@students.southwales.ac.uk>
2025-03-18 10:18:02 +0300

    Add the database connection code in the main file app.js

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ import apiRoutes from "./routes/api-routes.js"
 // Tell the app to use the routes we define in the api-routes.js file.
 app.use('/api', apiRoutes);
 /*--- This is the end of the new code ---*/
+
+//connect to mongoose and set connection variable
+mongoose.connect(config.DBHost, {});
+//added check for DB connection
+if(!mongoose.connection){
+	console.log("Error connecting DB");
+}else{
+	console.log("DB Connection Successful")
+}
+
 //set up the port number. Defaults to 3000 when we are not using a cloud platform
 const PORT = process.env.PORT || 3000
 
