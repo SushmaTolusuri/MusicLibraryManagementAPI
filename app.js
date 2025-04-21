8ccdcbe00236a1a7561403d1f2e1f266bc25bf00
SushmaTolusuri <30135057@students.southwales.ac.uk>
2025-04-21 18:57:57 +0300

    Update app.js to allow cors requests from the Web Application

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
 app.use(express.json());
 /*--- This is the new code ---*/
@@ -9,6 +11,12 @@ app.use(bodyParser.urlencoded({
 extended: true
 }));
 app.use(bodyParser.json())
+app.use(cors({
+    credentials: true,
+    preflightContinue: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
+    origin: true
+}));
 // Import routes after bodyParser has been initialised.
 // Here is where we link to our api-routes.js file.
 import apiRoutes from "./routes/api-routes.js"
